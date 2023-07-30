f0f843fc93b4f22c6ce8f398c1530e3b3d49bc15
MirzaAqmal <132419665+MirzaAqmal@users.noreply.github.com>
2023-07-30 15:11:44 +0800

    Create Kedai_Bunga.js

---
+++ Kedai_Bunga.js
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Kedai Bunga XYZ</title>
+        <script>
+            function tambahKeTroli() {
+                // Kode untuk menguruskan tindakan tambah ke troli 
+                alert ('Produk telah ditambah ke troli!');
+            }
+        </script>
+    </head>
+    <body>
+        <h1>Selamat Datang ke Kedai Bunga XYZ</h1>
+        <h2>Senarai Produk</h2>
+        <div class="produk">
+            <img src="c:\Users\USER\Downloads\download.jpeg" alt="Bunga Mawar">
+            <h3>Bunga Mawar</h3>
+            <p>Harga: RM 10.00</p>
+            <button onclick="tambahKeTroli()">Tambah ke Troli</button>
+        </div>
+        <div class="produk">
+            <img src="c:\Users\USER\Downloads\download (1).jpeg" alt="Bunga Anggerik">
+            <h3>Bunga Anggerik</h3>
+            <p>Harga: RM 15.00</p>
+            <button onclick="tambahKeTroli()">Tambah ke Troli</button>
+        </div>
+    </body>
+</html>
